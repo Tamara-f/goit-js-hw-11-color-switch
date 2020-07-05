8fb0beb72ad2c6b19a35c340d1fe81d77774e102
tamara-f <filatovatam@ukr.net>
2020-07-05 19:34:27 +0300

    new random

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const colors = [
   '#795548',
 ];
 
+let timerId = null;
+let index;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -15,19 +18,21 @@ const bodyRef = document.querySelector('body');
 const startbtnRef = document.querySelector('button[data-action="start"]');
 const stopbtnRef = document.querySelector('button[data-action="stop"]');
 
-let timerId = null;
-
 startbtnRef.addEventListener('click', () => {
   startbtnRef.disabled = true;
   timerId = setInterval(() => {
-    const index = randomIntegerFromInterval(0, Number(colors.length) - 1);
+    console.log(index);
+    const newindex = randomIntegerFromInterval(0, Number(colors.length) - 1);
+    newindex !== index ? (index = newindex) : (index = newindex - 1);
+    if (index === -1) {
+      index = newindex + 1;
+    }
     const bg = colors.find(color => colors.indexOf(color) === index);
     console.log(bg);
     bodyRef.style = `background-color:${bg}`;
     startbtnRef.disabled = true;
   }, 1000);
 });
-
 stopbtnRef.addEventListener('click', () => {
   startbtnRef.disabled = false;
   clearInterval(timerId);
